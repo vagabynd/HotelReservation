948fc3f06fc0703d10635913b8fe40a7c4f138bd
Evgeny Rotar <vagaabund@mail.ru>
2018-10-19 22:42:06 +0300

    clean-up

--- controller/src/main/java/com/evgen/controller/ReservationController.java
+++ controller/src/main/java/com/evgen/controller/ReservationController.java
@@ -1,7 +1,5 @@
 package com.evgen.controller;
 
-import java.text.ParseException;
-
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
